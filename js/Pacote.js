6b9bcca4d86345c60835eafa910cc600ba994645
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-06-18 16:31:22 -0300

    :construction: feat: destinatario e remetente

---
+++ js/Pacote.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Pacote = void 0;
+class Pacote {
+    constructor(numeroSequencia, dado) {
+        this.numeroSequencia = numeroSequencia;
+        this.dado = dado;
+    }
+}
+exports.Pacote = Pacote;
